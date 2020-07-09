4f67f7bb4e9b19e09c897f4571d84be132992bb3
anushka <anushkabahugunaa@gmail.com>
2020-07-09 19:04:46 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes   = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://anushkabahuguna:" + process.env.MONGO_ATLAS_PASSWORD +  "@cluster0.b2irn.mongodb.net/yelp_camp?retryWrites=true&w=majority", 
+				 { useNewUrlParser: true,
+				  useUnifiedTopology: true,
+				  useCreateIndex : true}).then(() => {
+	console.log("connected to db");
+}).catch(err =>{
+	console.log(err.message);
+});
+
+
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 app.use(express.static( __dirname + "/public"));
